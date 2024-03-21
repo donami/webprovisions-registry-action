c80ac28aa50aca90879ae07648a2b6210b767f5a
donami <d0namiz@gmail.com>
2024-03-21 22:58:09 +0100

    Add registry URL input to action.yml

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const core = require('@actions/core');
 const { promises: fs } = require('fs');
 
+const getScopes = (data, registryUrl, token) => {
+  const regex = /# scope\((.*)\)/g;
+  const registry = registryUrl.replace('https://', '').replace('http://', '');
+
+  let matches = [];
+  let output = [];
+
+  while ((matches = regex.exec(data))) {
+    const scope = matches[1];
+
+    const parts = scope.split(':');
+
+    if (parts[1]) {
+      const organizationKey = parts[1];
+
+      output.push(
+        `//${registry}/api/packages/${organizationKey}/npm/:_authToken=${token}`
+      );
+    }
+  }
+
+  return output;
+};
+
 const main = async () => {
   const token = core.getInput('registry-token');
+  const registryUrl = core.getInput('registry-url');
 
   if (!token || !token.length) {
     return;
@@ -11,15 +36,15 @@ const main = async () => {
   await fs.access('./.npmrc', fs.constants.F_OK);
   const data = await fs.readFile('./.npmrc', 'utf-8');
 
-  console.log('data', data);
+  const scopes = getScopes(data, registryUrl, token);
 
-  var result = data.replace(/deploykey/g, token);
+  const authString = scopes.join('\n');
 
-  console.log('result', result);
+  const output = `${data}\n${authString}`;
 
-  await fs.writeFile('./.npmrc', result, 'utf-8');
+  console.log('output', output); // TODO: remove
+
+  await fs.writeFile('./.npmrc', output, 'utf-8');
 };
 
 main().catch((err) => core.setFailed(err.message));
-
-console.log('Hello World!');
