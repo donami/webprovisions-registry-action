d4cd9f6bb5bf849f6c126cdf480ec5feedc76dcc
donami <d0namiz@gmail.com>
2024-03-21 20:14:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const core = require('@actions/core');
-
-try {
-  console.log(`Hello World!`);
-} catch (error) {
-  // Handle errors and indicate failure
-  core.setFailed(error.message);
-}
+console.log('Hello World!');
